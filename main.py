6478efc57c59cf7493273c70766060f70a41f851
Your Name <you@example.com>
2026-03-01 23:03:04 +0300

    Сделал лабу короче

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-
+import pandas as pd #https://www.kaggle.com/datasets/rishisukumar/student-screen-time-vs-cgpa-analysis-2026
+df = pd.read_csv("G:/Student_Performance_2026.csv")
+print(df.info())
+print(df.isnull().sum())
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+cols = df.drop(columns=['student_ID','Gender']).columns
+df[cols]= scaler.fit_transform(df[cols])
+df = pd.get_dummies(df, columns=['Gender'], drop_first=True)
+print(df.info())
+print(df.head())
