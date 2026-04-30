21e706953966c6be6af4276734a82434a7c5816e
Vubububub <kirillzoteev339@gmail.com>
2026-04-30 18:29:47 +0300

    добавил MLPClassifier

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_curve, auc
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.neural_network import MLPClassifier
+
 
 df = pd.read_csv("D:/spam_ham_dataset.csv")
 print(df.info())
@@ -18,7 +19,14 @@ X = df["text"]
 y = df["label_num"]
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=42,stratify=y)
 
-model = Pipeline([ ("tfidf", TfidfVectorizer(lowercase=True,stop_words="english")),("clf", LogisticRegression(max_iter=1000))])
+model = Pipeline([ ("tfidf", TfidfVectorizer(lowercase=True,stop_words="english")),
+                   ("nn", MLPClassifier(
+                       hidden_layer_sizes=(64,32),
+                        activation="relu",
+                       solver="adam",
+                       max_iter=5,
+                       early_stopping=True,
+                       random_state=42))])
 
 model.fit(X_train, y_train)
 
@@ -36,7 +44,6 @@ print("Confusion matrix:\n", cm)
 fpr, tpr, thresholds = roc_curve(y_test, prob)
 roc_auc = auc(fpr, tpr)
 print("\nROC-AUC:", roc_auc)
-print("Pobability of spam:", prob)
 plt.figure(figsize=(7,6))
 plt.plot(fpr, tpr, label=f"(AUC={roc_auc:.3f})")
 plt.plot([0,1], [0,1], linestyle='--')
